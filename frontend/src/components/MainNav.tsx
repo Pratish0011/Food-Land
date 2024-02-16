917d5a470e529d9a2082dacb04ecb1f778920522
Pratish Bhandari <pratishbhandari2@gmail.com>
2024-02-16 16:56:51 +0545

    Finished Homepage Design

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold hover:text-orange-500 hover:bg-orange-50 outline outline-orange-500"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;
