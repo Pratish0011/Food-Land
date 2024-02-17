2eb68ca7b98ee34c7efec655eed6073855ca0310
Pratish Bhandari <pratishbhandari2@gmail.com>
2024-02-17 16:23:18 +0545

    Auth0 login and logout

---
+++ frontend/src/components/MobileNavLinks.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+import { Button } from "./ui/button"
+import { useAuth0 } from "@auth0/auth0-react"
+
+
+const MobileNavLinks = () => {
+    const {logout} = useAuth0()
+  return (
+    <>
+      <Link className="flex bg-white items-center font-bold hover:text-orange-500" to="/user-profile">Your Profile</Link>
+      <Button className="flex items-center px-3 font-bold hover:bg-gray-500"
+      onClick={()=> logout()}
+      >
+        Log Out
+      </Button>
+    </>
+  )
+}
+
+export default MobileNavLinks
